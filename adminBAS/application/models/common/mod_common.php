e93c581e76f16d08b601bdb3c59b8e95a0c31a1f
Shahid Umar <shahid.umar@gmail.com>
2018-07-30 15:57:42 +0500

    Common

--- adminBAS/application/models/common/mod_common.php
+++ adminBAS/application/models/common/mod_common.php
@@ -518,7 +518,7 @@ class mod_common extends CI_Model {
 		
 		$this->db->dbprefix('messages');
 		$this->db->where('message_status',0);
-		$this->db->group_by('message_id');
+		//$this->db->group_by('message_id');
 		//$this->db->order_by('message_status', ASC);
 		$this->db->order_by('id', DESC);
 		$this->db->where('to',$this->session->userdata('admin_id'));
